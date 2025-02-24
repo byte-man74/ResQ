63a3508ac741df6cff0f68e2dabb352967dd2552
Shereef Habib <sheryf.534@gmail.com>
2025-02-24 10:07:08 +0100

    Add report service layer with CRUD operations and error handling

---
+++ server/internal/service/report_service.go
@@ -0,0 +1,59 @@
+package service
+
+import (
+	"fmt"
+	"example.com/resq/server/internal/models"
+	"example.com/resq/server/internal/repository"
+)
+
+
+
+type ReportService interface {
+	CreateReport(report *models.Report) (*models.Report, error)
+	EditReport(id string, report *models.Report) (*models.Report, error)
+	ViewReport(id string) (*models.Report, error)
+}
+
+
+type reportService struct {
+	repo repository.ReportRepository
+}
+
+
+
+func NewReportService (repo repository.ReportRepository) ReportService {
+	return &reportService{repo: repo}
+}
+
+
+func (r *reportService) CreateReport(report *models.Report) (*models.Report, error){
+	createdReport, err := r.repo.CreateReport(report)
+
+	if err != nil {
+		return nil, fmt.Errorf("unable to create report %w", err)
+	}
+
+	return createdReport, nil
+}
+
+
+func (r *reportService) EditReport(id string, report *models.Report) (*models.Report, error) {
+	editedReport, err  := r.repo.EditReport(id, report)
+
+	if err != nil {
+		return nil, fmt.Errorf("unable to edit report %w", err)
+	}
+
+	return editedReport, nil
+}
+
+
+func (r *reportService) ViewReport(id string) (*models.Report, error) {
+	report, err := r.repo.ViewReport(id)
+
+	if err != nil {
+		return nil, fmt.Errorf("unable to view report %w", err)
+	}
+
+	return report, nil
+}
